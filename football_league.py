db09437b6e89cca553b2b0a8b4684697052c28fa
“innocent <rockcoolsaint@gmail.com>
2017-08-09 13:34:39 +0100

    line trimming

--- football_league.py
+++ football_league.py
@@ -4,18 +4,21 @@ file = open("./football-league-results.txt", "r")
 smallest_team = "Kano Pillars"
 smallest_diff = 43
 
-def file_loop(content,small_team,small_diff,gf,ga,fname,lname):
-
+def file_loop(content,small_team,small_diff,gf,ga,fname=None,lname=None):
+	
 	for index, line in enumerate(content, 1):
-		f = re.split('\s*', line)
+		if index > 1:
+			f = line.lstrip()
+			f = f.rstrip()
+			f = re.split('\s*', f)
 
-		if index > 1 and len(f) >= 13 :
+		if index > 1 and re.match(r'[^-]+',f[0]):	
 			a = int(f[gf])
 			b = int(f[ga])
 			diff = a - b
 			if diff < small_diff:
-				smallest_diff = diff
+				small_diff = diff
 				small_team = f[fname] + " "  + f[lname]
-	print "Team with least goals difference: ",small_team
+	print "Team with least goals difference: ",small_team, small_diff
 
-file_loop(file,smallest_team,smallest_diff,8,10,2,3)
+file_loop(file,smallest_team,smallest_diff,7,9,1,2)
